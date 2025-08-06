6c4ef78b3432572e816e85897273974a7623840f
solar-path <itgroup.luck@gmail.com>
2025-08-07 00:11:55 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+
+
+        Route::get('/', function () {
+            return Inertia::render('welcome');
+        })->name('home');
+
+        Route::middleware(['auth', 'verified'])->group(function () {
+            Route::get('dashboard', function () {
+                return Inertia::render('dashboard');
+            })->name('dashboard');
+        });
+
+    });
+}
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
